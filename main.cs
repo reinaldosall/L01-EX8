0de858727aa7c1e1cc1bf18dc0ba712d43dd21e5
Reinaldo Saldanha <82273780+reinaldosall@users.noreply.github.com>
2021-08-23 22:32:27 +0000

    Reformulado cabeçalho do exercício

--- main.cs
+++ main.cs
@@ -3,8 +3,11 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
     /*
-    8- Implemente um código em C# que leia um  número inteiro (assuma números com apenas três dígitos) e imprima a saída com as centenas, dezenas e unidades conforme o exemplo. Ex.: número lido 769Saída: 7 centenas –6 dezenas –9 unidades
+    8- Implemente um código em C# que leia um  número inteiro (assuma números com 
+    apenas três dígitos) e imprima a saída com as centenas, dezenas e unidades 
+    conforme o exemplo. Ex.: número lido 769Saída: 7 centenas –6 dezenas –9 unidades
     */
+
     Console.WriteLine ("Digite um número inteiro com 3 dígitos");
     int nmrPedido = int.Parse(Console.ReadLine());
 
@@ -12,7 +15,7 @@ class MainClass {
     int dezena = (nmrPedido % 100) /10;
     int unidade = (nmrPedido % 100) %10;
 
-    Console.WriteLine ($"{centena} centenas - {dezena} dezenas - {unidade} unidades.");
+    Console.WriteLine ($"{centena} centenas - {dezena} dezenas - {unidade} unidades.");    
     
   }
 }
